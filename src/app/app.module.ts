2c659db65c7aae620a1410203d04f0c87bac1c04
cacero95 <cacero95@hotmail.com>
2019-04-20 12:38:40 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { File } from '@ionic-native/file/ngx';
+// social networks
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -16,6 +20,8 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    SocialSharing,
+    File,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+import { File } from '@ionic-native/file/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,65 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  text:string = "this is my first shared";
+  url:'https://ionicacademy.com';
+  constructor(private shared:SocialSharing,
+    private file:File){
+
+  }
+
+  sharedTwitter(){
+    this.shared.shareViaTwitter(null,`https://i.ytimg.com/vi/jeq8DGH1CSU/maxresdefault.jpg`).then(()=>{
+      console.log('voy por buen camino');
+      
+    }).catch(err=>{
+      console.log('la jodi');
+    })
+  }
+  async resolveLocalDirectory(){
+    console.log('file at:' + this.file);
+    
+    return this.file.copyFile(`${this.file.applicationDirectory}assets/img`
+    ,'kyoka.png',
+     this.file.cacheDirectory,
+     `${new Date().getTime()}.png`);  
+  }
+  removeTempFile(name){
+    this.file.removeFile(this.file.cacheDirectory,name);
+  }
+  async sharedFacebook(){
+    
+    this.shared.shareViaFacebook(null,'https://i.ytimg.com/vi/jeq8DGH1CSU/maxresdefault.jpg',this.url ).then(()=>{
+      console.log('voy por buen camino');
+      
+    }).catch(err=>{
+      console.log('la jodi');
+      
+    })
+    
+  }
+  async sharedEmail(){
+    
+    
+    this.shared.shareViaEmail('this is my message','probe subject',['cacero95@gmail.com'], null,null,'https://i.ytimg.com/vi/jeq8DGH1CSU/maxresdefault.jpg').then(()=>{
+    console.log('voy por buen camino');
+    
+    }).catch(err=>{
+      console.log('la jodi');
+      
+    })
+  }
+  sharedWhatapp(){
+    
+    
+    this.shared.shareViaWhatsApp(this.text,null,this.url).then(()=>{
+    console.log('voy por buen camino');
+    
+    }).catch(err=>{
+      console.log('la jodi');
+      
+    })
+  }
 
 }
